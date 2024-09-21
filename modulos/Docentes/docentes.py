ac5a29ddf01dc1215e4c8f1c07a6974e22e81727
Brayanestiv1 <cuentaestudio1504@gmail.com>
2024-09-21 15:56:09 -0500

    hice la matris de materias y agrege a los docentes ese dicionario, faltaria exportarlo

--- modulos/Docentes/docentes.py
+++ modulos/Docentes/docentes.py
@@ -1,13 +1,27 @@
 import json
+import os
 
 def lenCedula():
     while True:
-        cedula = int(input("Ingrese la celuda del docente: "))
-        if len(str(cedula)) <= 10:
-            return int(cedula)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        cedula = input("Ingrese la cédula del docente (10 dígitos): ")
+        
+        if len(cedula) == 10 and cedula.isdigit():
+            return int(cedula)  # Retorna la cédula válida
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')  # Limpia la pantalla
+            if not cedula.isdigit():
+                print(">>> Error. Debe ingresar un número.")
+                input("Presione cualquier tecla para volver a ingresar...")
+            else:
+                os.system('cls' if os.name == 'nt' else 'clear')  # Limpia la pantalla
+                print("La cédula debe tener exactamente 10 dígitos y contener solo números.")
+                input("Presione cualquier tecla para volver a ingresar...")
+            
 docentes = {
     "nombre" : input("Ingrese un nombre: "),
     "cedula" : lenCedula()
+    "materia" : lenMateria()
 }
 
 # Guardar en un archivo JSON
