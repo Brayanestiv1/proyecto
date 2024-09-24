150df32477b5b6c3f89b30d0323ce462b2dbaded
Brayanestiv1 <cuentaestudio1504@gmail.com>
2024-09-23 21:40:58 -0500

    .

--- interfaz/long.py
+++ interfaz/long.py
@@ -8,26 +8,32 @@ def encriptar_contraseña(contraseña):
 
 # Función de inicio de sesión
 def iniciar_sesion():
+    os.system('clear' if os.name != 'nt' else 'cls')
+    print("=======================================")
+    print("           INICIO DE SESIÓN           ")
+    print("=======================================")   
+
+
     usuario = input("Ingrese su usuario: ")
     while True:
         contraseña_guardada = cargar_contraseña()
         contraseña = input("Ingrese la contraseña: ")
 
         if encriptar_contraseña(contraseña) == contraseña_guardada:
-            print("Acceso concedido")
-            return
+            print("\nAcceso concedido\n")
+            break
         else:
-            os.system('clear')
+            os.system("clear")
             print(">>> Error. Contraseña incorrecta")
             input("Presione enter para continuar. . .")
-            os.system('clear')
+            os.system("clear")
 
 # Función para cargar la contraseña del archivo JSON
 def cargar_contraseña():
     try:
         with open("data/contraseña.json", "r") as archivo:
             datos = json.load(archivo)
-            return datos['contraseña']
+            return datos["contraseña"]
     except (FileNotFoundError, json.JSONDecodeError):
         # Crear la carpeta si no existe
         os.makedirs("data", exist_ok=True)
@@ -42,14 +48,15 @@ def guardar_contraseña(contraseña):
         with open("data/contraseña.json", "w") as archivo:
             json.dump({"contraseña": encriptar_contraseña(contraseña)}, archivo)
     except Exception as e:
-        print(f"Error al guardar la contraseña: {e}")
+        print(f">>> Error al guardar la contraseña: {e}")
 
 
 # Limpiar la consola antes de iniciar sesión
-os.system('clear' if os.name != 'nt' else 'cls')
+os.system("clear")
 
 # Iniciar sesión
 iniciar_sesion()
 
 # Mostrar el menú después de un inicio de sesión exitoso
+os.system("clear")
 menu.mostrar_menu  # Asegúrate de que la función menu() esté definida en 'interfaz'
